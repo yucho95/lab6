094139b1bccc153aa2d68d8762bd4c400b826350
Yubin Cho <yucho@ucsd.edu>
2017-02-16 13:14:17 -0800

    change

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -5,12 +5,21 @@ $(document).ready(function() {
 	initializePage();
 })
 
+
+function addProject(result){
+	var projectHTML = '<a href="#" class="thumbnail">' +
+    '<img src="' + result['image'] + '" class="img">' +
+    '<p>' + result['title'] + '</p>' +
+    '<p><small>' + result['date'] +
+    '<p><small>' + result['summary'] +
+    '</small></p></a>'; 
+    $(".details").append(projectHTML);
+}
 /*
  * Function that is called when the document is ready.
  */
 function initializePage() {
 	$('.project a').click(addProjectDetails);
-
 	$('#colorBtn').click(randomizeColors);
 }
 
@@ -26,6 +35,8 @@ function addProjectDetails(e) {
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
 
+	$.get("/project/"+idNumber, addProject);
+
 	console.log("User clicked on project " + idNumber);
 }
 
@@ -34,5 +45,15 @@ function addProjectDetails(e) {
  * and apply it
  */
 function randomizeColors(e) {
+	$.get("/palette", color);
 	console.log("User clicked on color button");
+}
+
+function color(result){
+	var colors = randomPalette['hex'];
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
 }
--- routes/palette.js
+++ routes/palette.js
@@ -3,5 +3,8 @@ var palettes = require('../palettes.json');
 exports.randomPalette = function(req, res) {
 	// get a random palette from the top ones
 	var randomPalette = palettes[Math.floor(palettes.length * Math.random())];
-	res.send('Your random palette is called: ' + randomPalette['title']);
+	//res.send('Your random palette is called: ' + randomPalette['title']);
+	var colors = randomPalette['hex'];
+	res.json(palettes.json);
+	res.send(randomPalette);
 }
